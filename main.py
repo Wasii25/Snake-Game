a6cd0012347e47e0a5807f8a5c2ac01c7834e6a5
Wasii25 <wasiullahrafeeq.s@gmail.com>
2024-09-27 18:47:52 +0530

    Day 20 - 21
    Day 21 and 22 in 100 days of code using python.
    A simple snake game that i created using python turtle library

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_on = True
 while game_on:
     screen.update()
@@ -30,41 +29,16 @@ while game_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_on = False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for segment in snake.segments[1:]:
+         if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
-
-
+ALIGNMENT = "center"
+FONT = ("courier", 20, "normal")
 class ScoreBoard(Turtle):
 
     def __init__(self):
@@ -13,10 +13,13 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score : {self.score}", align="center", font=("Arial", 18, "normal"))
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
