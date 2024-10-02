d37edd0443c5c85b9771f8244a740f3c58c7a161
Wasii25 <wasiullahrafeeq.s@gmail.com>
2024-10-02 22:47:28 +0530

    added a highscore label

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -23,11 +24,9 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def reset(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
-    def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
